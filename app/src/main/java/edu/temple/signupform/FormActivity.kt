69051f45dcb64dd8766f4cce9d2dcf7172820799
CoFlo35 <43580764+CoFlo35@users.noreply.github.com>
2021-09-21 18:00:07 -0400

    Added Implementation to check whether all fields are filled, if so there is a prompt displayed at the bottom confirming they have all been filled. If not there is a prompt asking the user to recheck their entries

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -13,31 +13,43 @@ class FormActivity() : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var Name :String
-        var emailAddress : String
-        val password : String
-        val passwordConfirmation: String
-
         val inputName = findViewById<TextView>(R.id.editTextViewName)
         val inputEmail = findViewById<TextView>(R.id.TextViewEmail)
         val inputPassword = findViewById<TextView>(R.id.editTextTextPassword)
         val inputPasswordConfirm = findViewById<TextView>(R.id.editTextTextPasswordConfirm)
         val saveButton = findViewById<Button>(R.id.buttonSave)
+        val displayText = findViewById<TextView>(R.id.TextViewDisplayMessage)
 
         saveButton.setOnClickListener() {
+            //redisplays whether an error should be attached to a TextView
             isNoEntry(inputName,"Name")
             isNoEntry(inputEmail, "Email")
             isNoEntry(inputPassword, "Password")
             isNoEntry(inputPasswordConfirm, "Confirm Password")
 
+            //Rechecks if any TextViews have an error attached
+            if(hasError(inputName) == true && hasError(inputEmail) == true
+                && hasError(inputPassword)==true && hasError(inputPasswordConfirm) == true){
+                displayText.text = "All Critera Filled"
+            }else{displayText.text = "Please Recheck all Fields"}
+
         }
 
-
     }
-
-    private fun isNoEntry(view: TextView, name:String) {
+    // Method Recieves a TextView and name of TextView to be displayed for error purposes
+    private fun isNoEntry(view: TextView, name:String){
         if(view.text.isNotEmpty()){}
         else{view.setError("${name} Cannot Be Blank")}
+    }
+    //Uses the .getError method to determine if there is an Error attached
+    // If there is an error return True
+    private fun hasError(view: TextView):Boolean{
+        if(view.getError() == null){return true}
+        else{return false}
+    }
+    //Method to check the literal strings of the Password and Confirmation Password
+    private fun textMatch(view1: TextView, view2:TextView):Boolean{
+        return view1.text.toString() == view2.text.toString()
     }
 
 
