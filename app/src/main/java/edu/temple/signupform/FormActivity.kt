866e1f69c4762800f05112f63cf33cfe01069f14
CoFlo35 <43580764+CoFlo35@users.noreply.github.com>
2021-09-21 16:51:18 -0400

    Simple Implementation of IsNoEntry, checks if field is blank, if field is blank -> provide error

--- app/src/main/java/edu/temple/signupform/FormActivity.kt
+++ app/src/main/java/edu/temple/signupform/FormActivity.kt
@@ -2,10 +2,43 @@ package edu.temple.signupform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
-class FormActivity : AppCompatActivity() {
+
+class FormActivity() : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var Name :String
+        var emailAddress : String
+        val password : String
+        val passwordConfirmation: String
+
+        val inputName = findViewById<TextView>(R.id.editTextViewName)
+        val inputEmail = findViewById<TextView>(R.id.TextViewEmail)
+        val inputPassword = findViewById<TextView>(R.id.editTextTextPassword)
+        val inputPasswordConfirm = findViewById<TextView>(R.id.editTextTextPasswordConfirm)
+        val saveButton = findViewById<Button>(R.id.buttonSave)
+
+        saveButton.setOnClickListener() {
+            isNoEntry(inputName,"Name")
+            isNoEntry(inputEmail, "Email")
+            isNoEntry(inputPassword, "Password")
+            isNoEntry(inputPasswordConfirm, "Confirm Password")
+
+        }
+
+
     }
+
+    private fun isNoEntry(view: TextView, name:String) {
+        if(view.text.isNotEmpty()){}
+        else{view.setError("${name} Cannot Be Blank")}
+    }
+
+
 }
